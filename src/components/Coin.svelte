bb1e87b0c7b1120e82a76ff3de135fb382071d39
Dan Collins <dan.collins.dev@gmail.com>
2024-09-29 14:34:39 -0400

    first commit before rewriting the main page's theme

---
+++ src/components/Coin.svelte
@@ -0,0 +1,30 @@
+<style>
+    .coin {
+        width: 8px;
+        height: 8px;
+        overflow: hidden;
+        background-color: red;
+    }
+
+    .coin-animation {
+        
+        translate: 0 -7px;
+        width: fit-content;
+        height: fit-content;
+        animation: spin 0.45s steps(6) infinite;
+    }
+
+    @keyframes spin {
+        from {
+            transform: translate3d(0px, 0, 0);
+        }
+        to {
+            transform: translate3d(-100%, 0, 0);
+        }
+    }
+</style>
+
+
+<div class="coin">
+    <img class="coin-animation" src="/coin-Sheet.png" alt="A spinning coin" />
+</div>
