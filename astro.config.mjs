f813e6a155debfb8550b1b78d9681c6ba0e81c3f
Dan Collins <dan.collins.dev@gmail.com>
2024-10-06 17:42:25 -0400

    Still working on blog layout, but its coming together

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dan-collins-dev.github.io',
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
 });
