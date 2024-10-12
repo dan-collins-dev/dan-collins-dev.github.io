867b650fd5bc4d537b6a5068e6c17ce7e9eb2013
Dan Collins <dan.collins.dev@gmail.com>
2024-10-11 20:58:41 -0400

    Added a component. Restructuring my components directory

---
+++ src/components/blog/BlogPostTitle.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let frontmatter;
+    const {title, description, author, pubDate} = frontmatter;
+    const date = new Date(pubDate); 
+</script>
+
+<article class="blog-card">
+    <h1 class="blog-label roboto-bold">{title}</h1>
+    <p class="roboto-thin">Written by {author}</p>
+    <p class="roboto-thin">
+        Date Published: {date.toUTCString().slice(0, 16)}
+    </p>
+</article>
